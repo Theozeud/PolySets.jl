07f7a04d3c47fb3d4f7e7c771bfee140e134d3b7
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-04-19 22:36:46 +0200

    Create evaluate.jl

---
+++ src/evaluate.jl
@@ -0,0 +1,86 @@
+"""
+    evaluate(ps::PolySet, x::Number)
+
+Evaluates a set of polynomials represented by `ps` at the scalar point `x`.
+
+# Arguments
+- `ps`: A `PolySet` object storing polynomial coefficients. Each row corresponds to one polynomial; columns represent increasing powers of `x`.
+- `x`: The scalar value at which all polynomials are evaluated.
+
+# Notes
+- Uses Horner's method for efficient polynomial evaluation.
+"""
+function evaluate(ps::PolySet, x::Number)
+    NewT = promote(typeof(x), eltype(ps))
+    y = zeros(NewT, size(ps,1))
+    evaluate!(y, ps, x)
+end
+
+
+"""
+    evaluate(ps::PolySet, x::Number)
+
+Evaluates a set of polynomials represented by `ps` at the scalar point `x`.
+
+# Arguments
+- `ps`: A `PolySet` object storing polynomial coefficients. Each row corresponds to one polynomial; columns represent increasing powers of `x`.
+- `x`: A vector of scalar inputs at which the polynomials are evaluated.
+
+# Notes
+- Uses Horner's method for efficient polynomial evaluation.
+"""
+function evaluate(ps::PolySet, x::AbstractVector)
+    NewT = promote(typeof(x), eltype(ps))
+    y = zeros(NewT, size(ps,1), length(x))
+    evaluate!(y, ps, x)
+end
+
+
+"""
+    evaluate!(y::AbstractVector, ps::PolySet, x::Number)
+
+Evaluates a set of polynomials represented by `ps` at the scalar point `x`, writing the result in-place into the vector `y`.
+
+# Arguments
+- `y`: Output vector (modified in-place), with length equal to the number of polynomials. After evaluation, `y[i]` contains the value of the i-th polynomial at `x`.
+- `ps`: A `PolySet` object storing polynomial coefficients. Each row corresponds to one polynomial; columns represent increasing powers of `x`.
+- `x`: The scalar value at which all polynomials are evaluated.
+
+# Notes
+- Uses Horner's method for efficient polynomial evaluation.
+"""
+function evaluate!(y::AbstractVector, ps::PolySet, x::Number)
+    @unpack coeffs = ps
+    @views y .= coeffs[:,end]
+    @inbounds for i ∈ maxdeg(ps):-1:1
+        @views coeffsi = coeffs[:,i]
+        @. y = y * x + coeffsi
+    end
+    y
+end
+
+"""
+    evaluate!(y::AbstractMatrix, ps::PolySet, x::AbstractVector)
+
+Evaluates a set of polynomials represented by `ps` at multiple scalar points given in `x`, writing the result in-place into the matrix `y`.
+
+# Arguments
+- `y`: Output matrix (modified in-place), of size `(n, m)` where `n` is the number of polynomials and `m == length(x)`. Column `j` of `y` contains the evaluations of all polynomials at `x[j]`.
+- `ps`: A `PolySet` object storing polynomial coefficients. Each row is a polynomial; columns represent increasing powers of `x`.
+- `x`: A vector of scalar inputs at which the polynomials are evaluated.
+
+# Notes
+- Uses Horner's method for fast evaluation.
+- Evaluation is vectorized over all input points.
+"""
+function evaluate!(y::AbstractMatrix, ps::PolySet, x::AbstractVector)
+    @unpack coeffs = ps
+    fill!(y, 0)
+    @views pend = coeffs[:,end]
+    y .+= pend
+    @inbounds for i ∈ maxdeg(ps):-1:1
+        @views coeffsi = coeffs[:,i]
+        @. y = y * x' + coeffsi
+    end
+    y
+end
