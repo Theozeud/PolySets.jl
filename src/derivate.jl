5b4d34627e8e0be25f8d64c81647b05f4217e39b
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-04-20 22:51:25 +0200

    Update derivate.jl

--- src/derivate.jl
+++ src/derivate.jl
@@ -5,7 +5,7 @@ Returns a new `PolySet` containing the derivatives of the polynomials in `ps`.
 
 Differentiation is performed in a fully vectorized way, and the result is returned as a new `PolySet`.
 """
-function derivate(ps::PolySet)
+function derivate(ps::PolysSet)
     ips = similar(ps, (npolys(ps),maxdeg(ps)))
     derivate!(ips, ps)
     ips
@@ -20,7 +20,7 @@ Computes the derivative of each polynomial in `ps` and stores the result in `ips
 The matrix `ips.coeffs` must have the same number of rows as `ps`, and at least `maxdeg(ps)` columns
 (since the result of a derivative has one fewer term). The operation is fully vectorized across rows.
 """
-function derivate!(ips::PolySet, ps::PolySet)
+function derivate!(ips::PolysSet, ps::PolysSet)
     fill!(ips.coeffs, 0)
     @views vips = ips.coeffs[:,1:maxdeg(p)]
     vips .= ps[:,2:end] 
