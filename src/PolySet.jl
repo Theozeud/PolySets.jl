d52398a85b58a4faf042e498117bfb18f4fd975f
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-04-20 23:34:01 +0200

    Update PolySet.jl

--- src/PolySet.jl
+++ src/PolySet.jl
@@ -4,9 +4,17 @@ module PolySet
     using SparseArrays
     using UnPack
 
+    export  PolysSet, 
+            allocate_PolysSet, npolys, maxdeg,
+            evaluate, evaluate!,
+            integrate, integrate!,
+            derivate, derivate!
+
     include("structure.jl")
     include("evaluate.jl")
     include("integrate.jl")
     include("derivate.jl")
+
+    export Monomials, Legendre, IntLegendre
     include("basis.jl")
 end
